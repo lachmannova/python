080899842f6c43ad570cc6ec61f8fab52766ef67
Klara Lachmannova <klara.lachmannova@gpjp.cz>
2024-04-09 13:18:02 +0200

    pridali jsme si kosticku

---
+++ kostka.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import random
+
+class Kostka:
+
+    def __init__(self, steny = 6):
+        self.__pocet_sten = steny
+
+    def __str__(self):
+        return f'Kostka s {self.__pocet_sten} stenami'
+
+    def hod(self):
+        return random.randint(1,self.__pocet_sten)
+
+
+    def getPocetSten(self):
+        return self.__pocet_sten
+
+k1 = Kostka()
+print(k1)
+print(k1.getPocetSten()+10)
+print(k1.hod())
+k2 = Kostka()
+print(k2)
